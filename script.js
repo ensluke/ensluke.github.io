8d9cd0fb3ca5b0ef951f10337b41fc35bf1006e5
Luke <luke.ens@ucalgary.ca>
2024-01-20 14:39:35 -0700

    added levels

--- script.js
+++ script.js
@@ -3,36 +3,57 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('game-container');
     const buttons = [];
-    let columnSums, columnBombs, rowSums, rowBombs, tiles, score, totalScore = 0;
-    refreshGame();
+    let columnSums, columnBombs, rowSums, rowBombs, tiles, score, totalScore = 0, rounds = 0, level = 0, levels, highScores, levelData;
+    loadData();
 
-    // let longPressTimer;
-
-    // function startLongPress(button) {
-    //     longPressTimer = setTimeout(() => {
-    //         handleRightClickMobile(button);
-    //     }, 500); // Adjust the duration of the long press as needed (in milliseconds)
-    // }
-
-    // function cancelLongPress() {
-    //     clearTimeout(longPressTimer);
-    // }
-
-    // function handleRightClickMobile(button) {
-    //     // Handle your right-click behavior here
-    //     button.classList.toggle('flagged');
-    // }
-
+    
+    function loadData() {
+        // Fetch the JSON file
+        fetch('data.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Access the array from the object
+            levels = data.levels;
+            highScores = data.topScores;
+            refreshGame();
+        })
+        .catch(error => {
+            console.error('Error fetching the JSON file:', error);
+        });
+    }
     
     function refreshGame() {
         container.innerHTML = '';
-        tiles = Array.from({ length: 25 }, () => getRandomInt(0, 3));
+        tiles = [];
         columnSums = [0, 0, 0, 0, 0];
         columnBombs = [0, 0, 0, 0, 0];
         rowSums = [0, 0, 0, 0, 0];
         rowBombs = [0, 0, 0, 0, 0];
-        score = 100;
-
+        score = 1;
+        
+        // Set tiles from json data
+        levelData = levels[level][getRandomInt(0, 4)];
+        for (let i = 0; i < levelData[0]; i++) {
+            tiles.push(2);
+        }
+        for (let i = 0; i < levelData[1]; i++) {
+            tiles.push(3);
+        }
+        for (let i = 0; i < levelData[2]; i++) {
+            tiles.push(0);
+        }
+        for (let i = tiles.length; i < 25; i++) {
+            tiles.push(1);
+        }
+        tiles = shuffleArray(tiles);
+        console.log(tiles);
+        
+        // Sum tile data
         for (let i = 0; i < 25; i++) {
             columnSums[i % 5] += tiles[i];
             rowSums[Math.floor(i / 5)] += tiles[i];
@@ -44,35 +65,52 @@ document.addEventListener('DOMContentLoaded', () => {
         
         // Initialize the game grid
         for (let i = 0; i < 35; i++) {
-            let fixedIndex = fixIndex(i);
-            if (fixedIndex >= 0) {
+            let adjustedIndex = adjustIndex(i);
+            if (adjustedIndex >= 0) {
                 const button = document.createElement('button');
                 button.className = 'game-square game-button';
-                button.addEventListener('click', () => handleButtonClick(button, fixedIndex));
-                // button.addEventListener('touchstart', () => startLongPress(button));
-                // button.addEventListener('touchend', cancelLongPress);
-                // button.addEventListener('touchcancel', cancelLongPress);
+                button.addEventListener('click', () => handleButtonClick(button, adjustedIndex));
                 button.addEventListener('contextmenu', (e) => handleRightClick(e, button));
                 container.appendChild(button);
                 buttons.push(button);
             } else {
+                let sum, bombs, sumIndex;
+                if (adjustedIndex == -1) {
+                    sumIndex = Math.floor(i / 6);
+                    sum = rowSums[sumIndex];
+                    bombs = rowBombs[sumIndex];
+                } else {
+                    sumIndex = i - 30;
+                    sum = columnSums[sumIndex];
+                    bombs = columnBombs[sumIndex];
+                }
                 const sumSquare = document.createElement('div');
                 const scoreSum = document.createElement('div');
                 const bombSum = document.createElement('div');
-                sumSquare.className = 'game-square';
+                
+                switch (sumIndex) {
+                    case 0:
+                        sumSquare.className = 'game-square red';
+                        break;
+                    case 1:
+                        sumSquare.className = 'game-square green';
+                        break;
+                    case 2:
+                        sumSquare.className = 'game-square orange';
+                        break;
+                    case 3:
+                        sumSquare.className = 'game-square blue';
+                        break;
+                    case 4:
+                        sumSquare.className = 'game-square violet';
+                        break;
+                    default:
+                        sumSquare.className = 'game-square';
+                }
                 scoreSum.className = 'sum-half sum-top';
                 bombSum.className = 'sum-half sum-bottom';
-                
-                let sum, bombs;
-                if (fixedIndex == -1) {
-                    sum = rowSums[Math.floor(i / 6)];
-                    bombs = rowBombs[Math.floor(i / 6)];
-                } else {
-                    sum = columnSums[i - 30];
-                    bombs = columnBombs[i - 30];
-                }
                 scoreSum.innerHTML = sum;
-                bombSum.innerHTML = `<img class="scaled-image" src="Voltorb.png">${bombs}`;
+                bombSum.innerHTML = `<img class="scaled-image" src="Voltorb.png" alt="0">${bombs}`;
                 
                 sumSquare.appendChild(scoreSum);
                 sumSquare.appendChild(bombSum);
@@ -82,21 +120,22 @@ document.addEventListener('DOMContentLoaded', () => {
         
         const resetButton = document.getElementById('reset-button');
         resetButton.addEventListener('click', resetGame);
-
-        // // Attach touch events to the buttons
-        // buttons.forEach((button) => {
-        //     button.addEventListener('click', () => handleButtonClick(button));
-        //     button.addEventListener('touchstart', () => startLongPress(button));
-        //     button.addEventListener('touchend', cancelLongPress);
-        //     button.addEventListener('touchcancel', cancelLongPress);
-        // });
+        document.getElementById('level-value').textContent = level+1;
     }
 
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function fixIndex(index) {
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+      }
+
+    function adjustIndex(index) {
         if (index % 6 == 5) return -1;  // rows
         if (index > 29) return -2;      // columns
         return Math.ceil(index - (index / 6)); // game grid
@@ -105,7 +144,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function handleButtonClick(button, index) {
         if (!button.classList.contains('flagged')) {
 
-            score *= tiles[index];
             if (tiles[index] > 0) {
                 button.textContent = tiles[index];
             } else {
@@ -115,7 +153,15 @@ document.addEventListener('DOMContentLoaded', () => {
             button.disabled = true;
             
             // Update the content of the html with the score
+            score *= tiles[index];
             document.getElementById('score-value').textContent = score;
+
+            // check if win or lose
+            if (score == levelData[3]) {
+                level++;
+                document.getElementById('level-value').textContent = level+1;
+                resetGame();
+            }
         }
     }
 
@@ -128,6 +174,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
     function resetGame() {
         totalScore += score;
+        rounds++;
         refreshGame();
         // Update the content of the html with the score
         document.getElementById('score-value').textContent = 0;
